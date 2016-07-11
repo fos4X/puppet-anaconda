b8278246810a8d166ce96aa819d2af725e0773a0
Philipp Tölke <philipp.toelke@fos4x.de>
2016-07-11 17:38:35 +0200

    allow pinning in envs

--- manifests/pinned.pp
+++ manifests/pinned.pp
@@ -1,10 +1,17 @@
 define conda::pinned(
-    $pins = []
+    $pins = [],
+    $env = undef,
 ){
     require conda
     include conda::params
 
-    file { "${conda::params::install_dir}/conda-meta/pinned":
+    if $env {
+        $filename = "${conda::params::install_dir}/envs/${env}/conda-meta/pinned"
+    } else {
+        $filename = "${conda::params::install_dir}/conda-meta/pinned"
+    }
+
+    file { $filename:
         content => template('conda/pinned.erb'),
     } ->  Package <| provider == conda or provider == conda_pip |>
 }
